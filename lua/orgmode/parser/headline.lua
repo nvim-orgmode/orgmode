3cfa484d71b4cc6336b2e27b6f086428a4950d60
Kristijan Husak <husakkristijan@gmail.com>
2021-05-14 23:52:43 +0200

    Add basic tag inheritance.

--- lua/orgmode/parser/headline.lua
+++ lua/orgmode/parser/headline.lua
@@ -12,7 +12,12 @@ function Headline:new(data)
   headline.content = {}
   headline.headlines = {}
   headline.todo_keyword = ''
-  headline.tags = {}
+  -- TODO: Add configuration for
+  -- - org-use-tag-inheritance
+  -- - org-tags-exclude-from-inheritance
+  -- - org-tags-match-list-sublevels
+  -- And add support for '#+FILETAGS'
+  headline.tags = {unpack(data.parent.tags or {})}
   setmetatable(headline, self)
   self.__index = self
   headline:parse_line()
@@ -40,9 +45,11 @@ function Headline:parse_line()
   end
   local tags = line:match(':.*:$')
   if tags then
-    self.tags = vim.tbl_filter(function(tag)
-      return tag:find('^[%w_@]+$')
-    end, vim.split(tags, ':'))
+    for _, tag in ipairs(vim.split(tags, ':')) do
+      if tag:find('^[%w_@]+$') and not vim.tbl_contains(self.tags, tag) then
+        table.insert(self.tags, tag)
+      end
+    end
   end
 end
 
--- tests/plenary/parser/parser_spec.lua
+++ tests/plenary/parser/parser_spec.lua
@@ -1,7 +1,9 @@
 local parser = require('orgmode.parser')
+
 describe('Parser', function()
   it('should parse lines', function()
     local lines = {
+      'Top level content',
       '* TODO Test orgmode',
       '** TODO Test orgmode level 2 :PRIVATE:',
       'Some content for level 2',
@@ -9,106 +11,118 @@ describe('Parser', function()
       'Content Level 3',
       '* DONE top level todo :WORK:',
       'content for top level todo',
-      '* DONE top level todo with multiple tags :OFFICE:PROJECT:',
+      '* TODO top level todo with multiple tags :OFFICE:PROJECT:',
       'multiple tags content, tags not read from content :FROMCONTENT:',
+      '** NEXT inherits tags without duplicates :OFFICE:NESTED:',
       '* NOKEYWORD Headline with wrong todo keyword and wrong tag format :WORK : OFFICE:',
     }
 
     local parsed = parser.parse(lines)
     assert.are.same({
       content = {},
-      headlines = { 2 },
+      headlines = { 3 },
       level = 1,
       line = "* TODO Test orgmode",
-      line_nr = 1,
+      line_nr = 2,
       parent = 0,
       type = "HEADLINE",
       todo_keyword = 'TODO',
       tags = {},
-    }, parsed.content[1])
+    }, parsed.items[2])
     assert.are.same({
-      content = { 3 },
-      headlines = { 4 },
+      content = { 4 },
+      headlines = { 5 },
       level = 2,
       line = "** TODO Test orgmode level 2 :PRIVATE:",
-      line_nr = 2,
-      parent = 1,
+      line_nr = 3,
+      parent = 2,
       type = "HEADLINE",
       todo_keyword = 'TODO',
       tags = {'PRIVATE'},
-    }, parsed.content[2])
+    }, parsed.items[3])
     assert.are.same({
       level = 2,
       line = "Some content for level 2",
-      line_nr = 3,
-      parent = 2,
+      line_nr = 4,
+      parent = 3,
       type = "CONTENT",
-    }, parsed.content[3])
+    }, parsed.items[4])
     assert.are.same({
-      content = { 5 },
+      content = { 6 },
       headlines = {},
       level = 3,
       line = "*** NEXT Level 3",
-      line_nr = 4,
-      parent = 2,
+      line_nr = 5,
+      parent = 3,
       type = "HEADLINE",
       todo_keyword = 'NEXT',
-      tags = {},
-    }, parsed.content[4])
+      tags = {'PRIVATE'},
+    }, parsed.items[5])
     assert.are.same({
       level = 3,
       line = "Content Level 3",
-      line_nr = 5,
-      parent = 4,
+      line_nr = 6,
+      parent = 5,
       type = "CONTENT",
-    }, parsed.content[5])
+    }, parsed.items[6])
     assert.are.same({
-      content = { 7 },
+      content = { 8 },
       headlines = {},
       level = 1,
       line = "* DONE top level todo :WORK:",
-      line_nr = 6,
+      line_nr = 7,
       parent = 0,
       type = "HEADLINE",
       todo_keyword = 'DONE',
       tags = {'WORK'},
-    }, parsed.content[6])
+    }, parsed.items[7])
     assert.are.same({
       level = 1,
       line = "content for top level todo",
-      line_nr = 7,
-      parent = 6,
-      type = "CONTENT",
-    }, parsed.content[7])
-    assert.are.same({
-      content = { 9 },
-      headlines = {},
-      level = 1,
-      line = "* DONE top level todo with multiple tags :OFFICE:PROJECT:",
       line_nr = 8,
+      parent = 7,
+      type = "CONTENT",
+    }, parsed.items[8])
+    assert.are.same({
+      content = { 10 },
+      headlines = { 11 },
+      level = 1,
+      line = "* TODO top level todo with multiple tags :OFFICE:PROJECT:",
+      line_nr = 9,
       parent = 0,
       type = "HEADLINE",
-      todo_keyword = 'DONE',
+      todo_keyword = 'TODO',
       tags = {'OFFICE', 'PROJECT'},
-    }, parsed.content[8])
+    }, parsed.items[9])
     assert.are.same({
       level = 1,
       line = "multiple tags content, tags not read from content :FROMCONTENT:",
-      line_nr = 9,
-      parent = 8,
+      line_nr = 10,
+      parent = 9,
       type = "CONTENT",
-    }, parsed.content[9])
+    }, parsed.items[10])
+    assert.are.same({
+      content = {},
+      headlines = {},
+      level = 2,
+      line = "** NEXT inherits tags without duplicates :OFFICE:NESTED:",
+      line_nr = 11,
+      parent = 9,
+      type = "HEADLINE",
+      todo_keyword = 'NEXT',
+      tags = {'OFFICE', 'PROJECT', 'NESTED'},
+    }, parsed.items[11])
     assert.are.same({
       content = {},
       headlines = {},
       level = 1,
       line = "* NOKEYWORD Headline with wrong todo keyword and wrong tag format :WORK : OFFICE:",
-      line_nr = 10,
+      line_nr = 12,
       parent = 0,
       type = "HEADLINE",
       todo_keyword = '',
       tags = {},
-    }, parsed.content[10])
+    }, parsed.items[12])
     assert.are.same(parsed.level, 0)
     assert.are.same(parsed.line_nr, 0)
     assert.are.same(parsed.lines, lines)
