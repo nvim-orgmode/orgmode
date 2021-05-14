5b86373af5aeb74c6f431d4c92922ce51e6d6ceb
Kristijan Husak <kris@getsquire.com>
2021-05-14 14:25:57 +0200

    Save all lines on root level object and keep only references to them.

--- lua/orgmode/parser/content.lua
+++ lua/orgmode/parser/content.lua
@@ -1,13 +1,16 @@
 local Content = {}
+local Types = require('orgmode.parser.types')
 
 function Content:new(data)
   data = data or {}
-  data.level = data.parent.level
-  data.parent = data.parent
-  data.line = data.line
-  setmetatable(data, self)
+  local content = { type = Types.CONTENT }
+  content.parent = data.parent.line_nr
+  content.level = data.parent.level
+  content.line = data.line
+  content.line_nr = data.line_nr
+  setmetatable(content, self)
   self.__index = self
-  return data
+  return content
 end
 
 return Content
