f7e9fc5f0030fa94c71b9a1664b1204cc11beb07
Kristijan Husak <husakkristijan@gmail.com>
2021-05-15 14:47:58 +0200

    Parse title and priority from headline.

--- lua/orgmode/parser/headline.lua
+++ lua/orgmode/parser/headline.lua
@@ -12,12 +12,13 @@ function Headline:new(data)
   headline.content = {}
   headline.headlines = {}
   headline.todo_keyword = ''
+  headline.priority = ''
+  headline.title = ''
   -- TODO: Add configuration for
   -- - org-use-tag-inheritance
   -- - org-tags-exclude-from-inheritance
   -- - org-tags-match-list-sublevels
-  -- And add support for '#+FILETAGS'
-  headline.tags = {unpack(data.parent.tags or {})}
+  headline.tags = {}
   setmetatable(headline, self)
   self.__index = self
   headline:parse_line()
@@ -36,21 +37,28 @@ end
 
 function Headline:parse_line()
   local line = self.line
-  line = line:gsub('^%*+%s', '')
+  line = line:gsub('^%*+%s+', '')
+
   for _, word in ipairs(todo_keywords) do
     if vim.startswith(line, word) then
       self.todo_keyword = word
       break
     end
   end
-  local tags = line:match(':.*:$')
+  self.priority = line:match(self.todo_keyword..'%s+%[#([A-Z0-9])%]') or ''
+  local tags = line:match(':.*:$') or ''
   if tags then
     for _, tag in ipairs(vim.split(tags, ':')) do
-      if tag:find('^[%w_@]+$') and not vim.tbl_contains(self.tags, tag) then
+      if tag:find('^[%w_%%@#]+$') and not vim.tbl_contains(self.tags, tag) then
         table.insert(self.tags, tag)
       end
     end
   end
+  local title = line
+  for _, exclude_pattern in ipairs({ self.todo_keyword, '%[#[A-Z0-9]%]', vim.pesc(':'..table.concat(self.tags, ':')..':') }) do
+    title = title:gsub(exclude_pattern, '')
+  end
+  self.title = vim.trim(title)
 end
 
 return Headline
