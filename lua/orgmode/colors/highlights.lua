0cb6db683cb23c5ad753d124758af056fb5540d7
Kristijan Husak <husakkristijan@gmail.com>
2021-05-30 21:43:52 +0200

    Improve syntax for org files.

---
+++ lua/orgmode/colors/highlights.lua
@@ -0,0 +1,52 @@
+local config = require('orgmode.config')
+local colors = require('orgmode.colors')
+local M = {}
+
+function M.define_agenda_colors()
+   local hl_map = M.get_agenda_hl_map()
+   local keyword_colors = colors.get_todo_keywords_colors()
+   local todo_keywords = config:get_todo_keywords()
+   for type, hlname in pairs(hl_map) do
+      local bold = ''
+      if vim.tbl_contains(todo_keywords.ALL, type) then
+         bold = ' gui=bold'
+      end
+      vim.cmd(string.format('hi %s guifg=%s%s', hlname, keyword_colors[type], bold))
+   end
+end
+
+function M.define_org_todo_keyword_colors()
+   local keyword_colors = colors.get_todo_keywords_colors()
+   local todo_keywords = config:get_todo_keywords()
+   vim.cmd(string.format([[syn match OrgTODO "\<\(%s\)\>" contained]], table.concat(todo_keywords.TODO, [[\|]])))
+   vim.cmd(string.format([[syn match OrgDONE "\<\(%s\)\>" contained]], table.concat(todo_keywords.DONE, [[\|]])))
+   vim.cmd(string.format('hi OrgTODO guifg=%s', keyword_colors.TODO))
+   vim.cmd(string.format('hi OrgDONE guifg=%s', keyword_colors.DONE))
+end
+
+function M.define_org_headline_colors()
+local headline_colors = {'Title', 'Constant', 'Identifier', 'Statement', 'PreProc', 'Type', 'Special', 'String'}
+local todo_keywords = config:get_todo_keywords()
+local all_keywords = table.concat(todo_keywords.ALL, [[\|]])
+for i, color in ipairs(headline_colors) do
+   local j = i
+   while j < 40 do
+      vim.cmd(string.format([[syn match OrgHeadlineLevel%d "^\*\{%d}\s\+\(\<\(%s\)\>\)\?.*$" contains=OrgTODO,OrgDONE]], j, j, all_keywords))
+      vim.cmd(string.format('hi default link OrgHeadlineLevel%d %s', j, color))
+      j = j + 8
+   end
+end
+end
+
+---@return table<string, string>
+function M.get_agenda_hl_map()
+   return {
+      TODO = 'OrgTODO',
+      DONE = 'OrgDONE',
+      deadline = 'OrgAgendaDeadline',
+      ok = 'OrgAgendaScheduled',
+      warning = 'OrgAgendaScheduledPast'
+   }
+end
+
+return M
--- syntax/orgagenda.vim
+++ syntax/orgagenda.vim
@@ -1,4 +1,4 @@
-lua require('orgmode.agenda.highlights').define_agenda_colors()
+lua require('orgmode.colors.highlights').define_agenda_colors()
 syn match OrgAgendaDay /^\w\+\s\+\d\+\s\w\+\s\d\d\d\d$/
 syn match OrgAgendaTags /:[^ ]*:$/
 hi OrgBold gui=bold
