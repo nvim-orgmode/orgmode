01eb70774ccba8ccd88e83f96cab8e5c7c9fe966
Kristijan Husak <husakkristijan@gmail.com>
2021-06-29 20:47:49 +0200

    Provide basic cterm colors and properly validate hex colors.

--- lua/orgmode/colors/init.lua
+++ lua/orgmode/colors/init.lua
@@ -11,13 +11,13 @@ end
 ---@param hlgroup string
 ---@return string
 function M.parse_hl_fg_color(hlgroup)
-   local bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(hlgroup)), 'bg', 'gui')
-   local fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(hlgroup)), 'fg', 'gui')
-   local normal_bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID('Normal')), 'bg', 'gui')
+   local bg = colors.validate(vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(hlgroup)), 'bg', 'gui'))
+   local fg = colors.validate(vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(hlgroup)), 'fg', 'gui'))
+   local normal_bg = colors.validate(vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID('Normal')), 'bg', 'gui'))
    local reverse = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(hlgroup)), 'reverse', 'gui')
    -- If only one color defined return that one
-   if bg == '' and fg ~= '' then return fg end
-   if fg == '' and bg ~= '' then return bg end
+   if not bg and fg then return fg end
+   if not fg and bg then return bg end
 
    if reverse == '' and bg ~= normal_bg then
       return bg
@@ -29,31 +29,40 @@ M.get_todo_keywords_colors = function()
    local error = M.parse_hl_fg_color('Error')
    local warning = M.parse_hl_fg_color('WarningMsg')
    local ok = M.parse_hl_fg_color('diffAdded')
-   if ok == '' then
+   if not ok then
       ok = M.parse_hl_fg_color('DiffAdd')
    end
-   if error == '' then
+   if not error then
      error = M.parse_hl_fg_color('ErrorMsg')
    end
 
-   if ok == '' or ok:sub(1, 1) ~= '#' then
+   if not ok then
      ok = '#00FF00'
    end
 
-   if warning == '' or warning:sub(1, 1) ~= '#' then
+   if not warning then
      warning = '#FF8C00'
    end
 
-   if error == '' or error:sub(1, 1) ~= '#' then
+   if not error then
      error = '#FF0000'
    end
 
    return {
-      TODO = error,
-      DONE = ok,
-      deadline = M.from_hex(error):lighten_by(0.1):to_rgb(),
-      ok = M.from_hex(ok):lighten_by(0.1):to_rgb(),
-      warning = M.from_hex(warning):lighten_by(0.1):to_rgb(),
+      TODO = { gui = error, cterm = 1 },
+      DONE = { gui = ok, cterm = 2 },
+      deadline = {
+        gui = M.from_hex(error):lighten_by(0.1):to_rgb(),
+        cterm = 9,
+      },
+      ok = {
+        gui = M.from_hex(ok):lighten_by(0.1):to_rgb(),
+        cterm = 10
+      },
+      warning = {
+        gui = M.from_hex(warning):lighten_by(0.1):to_rgb(),
+        cterm = 11,
+      }
    }
 end
 
