c18a5f235df145ba36f3660ac72fda7cb0f1526e
Kristijan Husak <husakkristijan@gmail.com>
2021-09-07 17:00:56 +0200

    Use silent when adding todo keywords to internal spellgood list.

--- lua/orgmode/org/syntax.lua
+++ lua/orgmode/org/syntax.lua
@@ -32,7 +32,7 @@ end
 local function add_todo_keywords_to_spellgood()
   local todo_keywords = config:get_todo_keywords().ALL
   for _, todo_keyword in ipairs(todo_keywords) do
-    vim.cmd(string.format('spellgood! %s', todo_keyword))
+    vim.cmd(string.format('silent! spellgood! %s', todo_keyword))
   end
 end
 
