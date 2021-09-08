531fc8f9026268f41f037cc75bad77b419f1b7d3
Kristijan Husak <husakkristijan@gmail.com>
2021-09-08 23:45:40 +0200

    Add a small delay when loading test fixtures to make sure it's loaded.

--- tests/plenary/ui/helpers.lua
+++ tests/plenary/ui/helpers.lua
@@ -1,11 +1,15 @@
+local Files = require('orgmode.parser.files')
 local function load_file(path)
   vim.cmd(string.format('e %s', path))
-  require('orgmode.parser.files').ensure_loaded()
+  Files.ensure_loaded()
+  vim.wait(500, function()
+    return Files.get(path) ~= nil
+  end, 5)
   vim.cmd(string.format('e %s', path))
 end
 
 local function load_file_content(content)
-  require('orgmode.parser.files').loaded = false
+  Files.loaded = false
   local fname = vim.fn.tempname() .. '.org'
   vim.fn.writefile(content or {}, fname)
   load_file(fname)
