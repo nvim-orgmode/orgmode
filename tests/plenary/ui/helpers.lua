9c07c850216bec62909a020fadae80a0e50da103
Kristijan Husak <husakkristijan@gmail.com>
2021-09-09 08:58:07 +0200

    Do not modify files loaded flag for tests.

--- tests/plenary/ui/helpers.lua
+++ tests/plenary/ui/helpers.lua
@@ -1,15 +1,13 @@
 local Files = require('orgmode.parser.files')
 local function load_file(path)
   vim.cmd(string.format('e %s', path))
-  Files.ensure_loaded()
-  vim.wait(500, function()
+  vim.wait(5000, function()
     return Files.get(path) ~= nil
   end, 5)
   vim.cmd(string.format('e %s', path))
 end
 
 local function load_file_content(content)
-  Files.loaded = false
   local fname = vim.fn.tempname() .. '.org'
   vim.fn.writefile(content or {}, fname)
   load_file(fname)
