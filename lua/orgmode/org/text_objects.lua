8870503d6ad6e19f7b77c1a71c563f8baaa9057a
Kristijan Husak <husakkristijan@gmail.com>
2021-10-26 18:51:46 +0200

    Fix text objects selection on last section.

--- lua/orgmode/org/text_objects.lua
+++ lua/orgmode/org/text_objects.lua
@@ -1,7 +1,17 @@
-local utils = require('orgmode.utils')
 local ts_utils = require('nvim-treesitter.ts_utils')
 local Files = require('orgmode.parser.files')
 local TextObjects = {}
+
+local function get_range_for_node_range(start_line, end_line, end_col)
+  -- Node ranges are 0 indexed
+  local start_range = start_line + 1
+  local end_range = end_line + 1
+  -- Sections range ends on next line with col value of 0, and we need to subtract 1 to get correct end line
+  if end_col == 0 and start_line ~= end_line then
+    end_range = end_range - 1
+  end
+  return { start_range = start_range, end_range = end_range }
+end
 
 local function get_current_section_range()
   local node = Files.get_node_at_cursor()
@@ -14,27 +24,27 @@ local function get_current_section_range()
   if not node then
     return
   end
-  local start_line, _, end_line, _ = node:range()
+  local start_line, _, end_line, end_col = node:range()
   local children = ts_utils.get_named_children(node)
-  local start_range = start_line + 1
-  local end_range = end_line
   if children[#children]:type() == 'section' then
     for _, child in ipairs(children) do
       if child:type() == 'section' then
-        local s, _, _, _ = child:range()
-        end_range = s
+        local s, _, _, ec = child:range()
+        end_line = s
+        end_col = ec
         break
       end
     end
   end
 
-  return start_range, end_range
+  return get_range_for_node_range(start_line, end_line, end_col)
 end
 
----@param start_range number
----@param end_range number
+---@param ranges table
 ---@param exclude_stars boolean
-local function do_selection(start_range, end_range, exclude_stars)
+local function do_selection(ranges, exclude_stars)
+  local start_range = ranges.start_range
+  local end_range = ranges.end_range
   local col = 1
   if exclude_stars then
     local _, offset = vim.fn.getline(start_range):find('^%*+%s*')
@@ -51,8 +61,7 @@ local function do_selection(start_range, end_range, exclude_stars)
 end
 
 local function current_heading(exclude_stars)
-  local start_range, end_range = get_current_section_range()
-  do_selection(start_range, end_range, exclude_stars)
+  do_selection(get_current_section_range(), exclude_stars)
 end
 
 local function current_subtree(exclude_stars)
@@ -66,12 +75,12 @@ local function current_subtree(exclude_stars)
   if not node then
     return
   end
-  local start_range, _, end_range, _ = node:range()
-  do_selection(start_range + 1, end_range, exclude_stars)
+  local start_range, _, end_range, end_col = node:range()
+  do_selection(get_range_for_node_range(start_range, end_range, end_col), exclude_stars)
 end
 
 local function current_heading_from_root(exclude_stars)
-  local _, end_range = get_current_section_range()
+  local end_range = get_current_section_range().end_range
   local node = Files.get_node_at_cursor()
   if not node then
     return
@@ -83,8 +92,8 @@ local function current_heading_from_root(exclude_stars)
     end
     node = parent
   end
-  local start_range, _, _, _ = node:range()
-  do_selection(start_range + 1, end_range, exclude_stars)
+  local start_range, _, _, end_col = node:range()
+  do_selection(get_range_for_node_range(start_range, end_range, end_col), exclude_stars)
 end
 
 local function current_subtree_from_root(exclude_stars)
@@ -99,8 +108,8 @@ local function current_subtree_from_root(exclude_stars)
     end
     node = parent
   end
-  local start_range, _, end_range, _ = node:range()
-  do_selection(start_range + 1, end_range, exclude_stars)
+  local start_range, _, end_range, end_col = node:range()
+  do_selection(get_range_for_node_range(start_range, end_range, end_col), exclude_stars)
 end
 
 function TextObjects.inner_heading()
