a4ec5a58212e017a7f0d623b55db656418d075c8
Kristijan Husak <husakkristijan@gmail.com>
2021-11-10 12:55:43 +0100

    Catch any errors and properly fallback to default formatexpr. Fixes #144.

--- lua/orgmode/org/format.lua
+++ lua/orgmode/org/format.lua
@@ -2,12 +2,12 @@ local Files = require('orgmode.parser.files')
 
 local function format()
   local line = vim.fn.line('.')
-  local item = Files.get_closest_headline(line)
-  if not item then
+  local ok, item = pcall(Files.get_closest_headline, line)
+  if not ok or not item then
     return 1
   end
 
-  if item and item.logbook and item.logbook.range:is_in_line_range(line) then
+  if item.logbook and item.logbook.range:is_in_line_range(line) then
     return item.logbook:recalculate_estimate(line)
   end
 
