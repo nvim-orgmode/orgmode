15f97ce269483b5abe9a34ef4762e04b3d1ed7d1
Kristijan Husak <husakkristijan@gmail.com>
2021-11-10 12:40:04 +0100

    Set fallback for formatexpr. Maybe fixes #144.

--- lua/orgmode/org/format.lua
+++ lua/orgmode/org/format.lua
@@ -4,12 +4,14 @@ local function format()
   local line = vim.fn.line('.')
   local item = Files.get_closest_headline(line)
   if not item then
-    return
+    return 1
   end
 
   if item and item.logbook and item.logbook.range:is_in_line_range(line) then
     return item.logbook:recalculate_estimate(line)
   end
+
+  return 1
 end
 
 return format
