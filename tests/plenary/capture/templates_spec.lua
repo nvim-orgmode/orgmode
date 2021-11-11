cf7bcf5461a9a33f5370643177084a0c77811289
Tray Dennis <tman1300@aol.com>
2021-11-11 10:29:09 -0500

    update template spec to account for clipboard

--- tests/plenary/capture/templates_spec.lua
+++ tests/plenary/capture/templates_spec.lua
@@ -3,8 +3,11 @@ local Templates = require('orgmode.capture.templates')
 describe('Capture template', function()
   local templates = Templates:new()
   it('should compile expression', function()
+    ---Backup and restore the clipboard
+    local clip_backup = vim.fn.getreg('+')
+    vim.fn.setreg('+', 'test')
     local result = templates:compile({
-      template = '* TODO\n%<%Y-%m-%d>\n%t\n%T--%T\n%<%H:%M>\n%<%A>',
+      template = '* TODO\n%<%Y-%m-%d>\n%t\n%T--%T\n%<%H:%M>\n%<%A>\n%x',
     })
 
     assert.are.same({
@@ -14,6 +17,9 @@ describe('Capture template', function()
       '<' .. os.date('%Y-%m-%d %a %H:%M') .. '>--<' .. os.date('%Y-%m-%d %a %H:%M') .. '>',
       os.date('%H:%M'),
       os.date('%A'),
+      'test',
     }, result)
+
+  vim.fn.setreg('+', clip_backup)
   end)
 end)
