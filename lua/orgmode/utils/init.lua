3394c94ca307750ea2b05ce4d3fcd15dc7c2a1c1
Branislav Gerazov <branislav.gerazov@gmail.com>
2021-11-28 21:17:12 +0100

    added debug printing function

--- lua/orgmode/utils/init.lua
+++ lua/orgmode/utils/init.lua
@@ -427,4 +427,31 @@ function utils.promisify(fn)
   return fn
 end
 
+---debug printing a table
+---source https://stackoverflow.com/questions/41942289/display-contents-of-tables-in-lua
+function utils.tprint (tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2 
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "   
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\r\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\r\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
+end
+
 return utils
