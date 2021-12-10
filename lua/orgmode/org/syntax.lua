2813244429f65edc29d2101b0be499b8d9e79f65
Kristijan Husak <husakkristijan@gmail.com>
2021-12-10 21:42:11 +0100

    Fix src block filetype detection. Closes #170.

--- lua/orgmode/org/syntax.lua
+++ lua/orgmode/org/syntax.lua
@@ -12,12 +12,18 @@ local function load_code_blocks()
     return
   end
 
-  for _, ft in ipairs(orgfile.source_code_filetypes) do
-    vim.cmd(string.format([[silent! syntax include @orgmodeBlockSrc%s syntax/%s.vim]], ft, ft))
-    vim.cmd([[unlet! b:current_syntax]])
-  end
+  local loaded_filetypes = {}
 
   for _, ft in ipairs(orgfile.source_code_filetypes) do
+    local ok, result = pcall(vim.api.nvim_get_runtime_file, string.format('syntax/%s.vim', ft), false)
+    if ok and #result > 0 then
+      vim.cmd(string.format([[silent! syntax include @orgmodeBlockSrc%s syntax/%s.vim]], ft, ft))
+      vim.cmd([[unlet! b:current_syntax]])
+      table.insert(loaded_filetypes, ft)
+    end
+  end
+
+  for _, ft in ipairs(loaded_filetypes) do
     vim.cmd(
       string.format(
         [[syntax region orgmodeBlockSrc%s matchgroup=comment start="^\s*#+BEGIN_SRC\ %s\s*.*$" end="^\s*#+END_SRC\s*$" keepend contains=@orgmodeBlockSrc%s,org_block_delimiter]],
