2787fac04327909ead40c656cc08e2d8d51d7008
Branislav Gerazov <branislav.gerazov@gmail.com>
2021-12-17 17:13:13 +0100

    trying without checking for different category

--- lua/orgmode/agenda/views/agenda.lua
+++ lua/orgmode/agenda/views/agenda.lua
@@ -50,9 +50,9 @@ local function sort_agenda_items(agenda_items)
     end
 
     if a.date:is_same(b.date) then
-      if a.headline:get_category() ~= b.headline:get_category() then
-        return category_inds[a.headline:get_category()] < category_inds[b.headline:get_category()]
-      end
+      -- if a.headline:get_category() ~= b.headline:get_category() then
+      return category_inds[a.headline:get_category()] < category_inds[b.headline:get_category()]
+      -- end
     end
 
     return a.headline_date:is_before(b.headline_date)
