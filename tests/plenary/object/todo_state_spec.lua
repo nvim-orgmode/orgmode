327fbb1d54ad62a7e1d1f6e5eb47b48f51504ca9
Jonas Gollenz <jgollenz@outlook.com>
2022-05-27 08:28:44 +0200

    Handle multiple substitutions in custom todo keyword

--- tests/plenary/object/todo_state_spec.lua
+++ tests/plenary/object/todo_state_spec.lua
@@ -18,11 +18,22 @@ describe('Todo state', function()
 
   it('should properly cycle through all defined custom states', function()
     config:extend({
-      org_todo_keywords = { 'TODO', 'WAITING', 'PROCESSING', '|', 'CONFIRM', 'DONE' },
+      org_todo_keywords = {
+        'TODO',
+        'WAITING',
+        'PROCESSING',
+        'HYPHEN-KEYWORD',
+        'MULTI-HYPHEN-KEYWORD',
+        '|',
+        'CONFIRM',
+        'DONE',
+      },
     })
     local next_state = TodoState:new({ current_state = 'TODO' })
     assert.are.same({ value = 'WAITING', type = 'TODO', hl = 'OrgTODO' }, next_state:get_next())
     assert.are.same({ value = 'PROCESSING', type = 'TODO', hl = 'OrgTODO' }, next_state:get_next())
+    assert.are.same({ value = 'HYPHEN-KEYWORD', type = 'TODO', hl = 'OrgTODO' }, next_state:get_next())
+    assert.are.same({ value = 'MULTI-HYPHEN-KEYWORD', type = 'TODO', hl = 'OrgTODO' }, next_state:get_next())
     assert.are.same({ value = 'CONFIRM', type = 'DONE', hl = 'OrgDONE' }, next_state:get_next())
     assert.are.same({ value = 'DONE', type = 'DONE', hl = 'OrgDONE' }, next_state:get_next())
     assert.are.same({ value = '', type = '' }, next_state:get_next())
@@ -32,6 +43,8 @@ describe('Todo state', function()
     assert.are.same({ value = '', type = '' }, prev_state:get_prev())
     assert.are.same({ value = 'DONE', type = 'DONE', hl = 'OrgDONE' }, prev_state:get_prev())
     assert.are.same({ value = 'CONFIRM', type = 'DONE', hl = 'OrgDONE' }, prev_state:get_prev())
+    assert.are.same({ value = 'MULTI-HYPHEN-KEYWORD', type = 'TODO', hl = 'OrgTODO' }, prev_state:get_prev())
+    assert.are.same({ value = 'HYPHEN-KEYWORD', type = 'TODO', hl = 'OrgTODO' }, prev_state:get_prev())
     assert.are.same({ value = 'PROCESSING', type = 'TODO', hl = 'OrgTODO' }, prev_state:get_prev())
     assert.are.same({ value = 'WAITING', type = 'TODO', hl = 'OrgTODO' }, prev_state:get_prev())
     assert.are.same({ value = 'TODO', type = 'TODO', hl = 'OrgTODO' }, prev_state:get_prev())
