dcc9784a68082dd5d964106b6030341016b06412
Jonas Gollenz <jgollenz@outlook.com>
2022-05-29 12:43:17 +0200

    Fix wrong function access & output refile error

--- lua/orgmode/capture/init.lua
+++ lua/orgmode/capture/init.lua
@@ -194,14 +194,19 @@ function Capture:_refile_content_with_fallback(lines, fallback_file, item)
   destination = vim.split(destination, '/', true)
 
   if not valid_destinations[destination[1]] then
+    if not default_file then -- we know that this comes from org_refile and not org_capture_refile
+        utils.echo_error("'" .. destination[1] .. "' is not a file specified in the 'org_agenda_files' setting. Refiling cancelled.")
+        return
+    end
     return self:_refile_to_end(default_file, lines, item)
   end
 
   local destination_file = valid_destinations[destination[1]]
-  if not destination[2] or destination[2] == '' then
+  local destination_headline = destination[2]
+  if not destination_headline or destination_headline == '' then
     return self:_refile_to_end(destination_file, lines, item)
   end
-  return self:refile_to_headline(destination_file, lines, item, destination[2])
+  return self:refile_to_headline(destination_file, lines, item, destination_headline)
 end
 
 ---@param destination_file string
@@ -216,7 +221,8 @@ function Capture:refile_to_headline(destination_file, lines, item, headline_titl
   end
 
   if not headline then
-    return self._refile_to_end(destination_file, lines, item)
+    utils.echo_info("headline '" .. headline_title .. "' does not exist in " .. destination_file .. ". Refiling to end of file instead.")
+    return self:_refile_to_end(destination_file, lines, item)
   end
 
   if item and item.level <= headline.level then
