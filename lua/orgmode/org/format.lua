2a386fc411c25154ec57b725ba65c5b494ba2b20
Kristijan Husak <husakkristijan@gmail.com>
2022-09-17 12:22:28 +0200

    Fix formatexpr and add align tags to it

--- lua/orgmode/org/format.lua
+++ lua/orgmode/org/format.lua
@@ -1,5 +1,30 @@
 local Files = require('orgmode.parser.files')
 local Table = require('orgmode.treesitter.table')
+local ts_org = require('orgmode.treesitter')
+
+local function format_line(linenr)
+  local line_text = vim.fn.getline(linenr)
+
+  if line_text:match('^%*+%s') then
+    local headline = ts_org.closest_headline({ linenr, 1 })
+    if headline then
+      headline:align_tags()
+      return true
+    end
+  end
+
+  if Table.format(linenr) then
+    return true
+  end
+
+  local ok, item = pcall(Files.get_closest_headline, linenr)
+  if ok and item and item.logbook and item.logbook.range:is_in_line_range(linenr) then
+    item.logbook:recalculate_estimate(linenr)
+    return true
+  end
+
+  return false
+end
 
 local function format()
   if vim.tbl_contains({ 'i', 'R', 'ic', 'ix' }, vim.fn.mode()) then
@@ -8,16 +33,17 @@ local function format()
     return 1
   end
 
-  local table = Table.format()
+  local start_line = vim.v.lnum
+  local end_line = vim.v.lnum + vim.v.count - 1
+  local formatted = false
 
-  if table then
-    return 0
+  for linenr = start_line, end_line do
+    local line_formatted = format_line(linenr)
+    formatted = formatted or line_formatted
   end
 
-  local line = vim.fn.line('.')
-  local ok, item = pcall(Files.get_closest_headline, line)
-  if ok and item and item.logbook and item.logbook.range:is_in_line_range(line) then
-    return item.logbook:recalculate_estimate(line)
+  if formatted then
+    return 0
   end
 
   return 1
