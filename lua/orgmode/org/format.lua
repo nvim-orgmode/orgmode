4ce3efbc75c34af1ec6958c64014e03ac8082de5
Jonas Gollenz <jgollenz@outlook.com>
2022-09-17 12:30:44 +0200

    WIP

--- lua/orgmode/org/format.lua
+++ lua/orgmode/org/format.lua
@@ -1,6 +1,7 @@
 local Files = require('orgmode.parser.files')
 local ts_org = require('orgmode.treesitter')
 local Table = require('orgmode.treesitter.table')
+local util = require('orgmode.utils')
 
 local function format()
   if vim.tbl_contains({ 'i', 'R', 'ic', 'ix' }, vim.fn.mode()) then
@@ -21,12 +22,25 @@ local function format()
     return item.logbook:recalculate_estimate(line)
   end
 
-  if vim.fn.getline(line):match('^%*') then
-    ts_org.closest_headline():align_tags()
+  local start_line = vim.v.lnum
+  local end_line = start_line + vim.v.count
+  local formatted_headlines = 0
+
+  util.echo_info('lnum: ' .. vim.v.lnum .. ', count: ' .. vim.v.count)
+
+  for line_num = start_line, end_line, 1 do
+    if vim.fn.getline(line_num):match('^%*') then
+      ts_org.closest_headline():align_tags()
+      formatted_headlines = formatted_headlines + 1
+    end
+  end
+
+  -- it could be that there were no headlines in the selection|motion
+  if formatted_headlines > 0 then
     return 0
   end
 
-  return 1
+  -- return 1
 end
 
 return format
