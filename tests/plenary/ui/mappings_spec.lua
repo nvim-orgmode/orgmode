031bf0008e150597e6246aef1055533e2a9c9579
Sebastian Flügge <seflue@web.de>
2023-07-03 22:28:42 +0200

    test: add approval tests for hyperlinks

--- tests/plenary/ui/mappings_spec.lua
+++ tests/plenary/ui/mappings_spec.lua
@@ -1880,6 +1880,70 @@ describe('Mappings', function()
     }, vim.api.nvim_buf_get_lines(0, 0, 6, false))
   end)
 
+  it('should follow link to given headline in given org file', function()
+    local target_path = helpers.load_file_content({
+      '* Test hyperlink',
+      ' - some',
+      ' - boiler',
+      ' - plate',
+      '** target headline',
+      '   - more',
+      '   - boiler',
+      '   - plate',
+    })
+    vim.cmd([[norm w]])
+    helpers.load_file_content({
+      string.format('This link should lead to [[file:%s::*target headline][target]]', target_path),
+    })
+    vim.cmd([[norm w]])
+    vim.fn.cursor(1, 30)
+    vim.cmd([[norm ,oo]])
+    assert.is.same('** target headline', vim.api.nvim_get_current_line())
+  end)
+
+  it('should follow link to headline of given custom_id in given org file', function()
+    local target_path = helpers.load_file_content({
+      '* Test hyperlink',
+      ' - some',
+      ' - boiler',
+      ' - plate',
+      '** headline of target custom_id',
+      '   :PROPERTIES:',
+      '   :CUSTOM_ID: target',
+      '   :END:',
+      '   - more',
+      '   - boiler',
+      '   - plate',
+    })
+    vim.cmd([[norm w]])
+    helpers.load_file_content({
+      string.format('This link should lead to [[file:%s::#target][target]]', target_path),
+    })
+    vim.cmd([[norm w]])
+    vim.fn.cursor(1, 30)
+    vim.cmd([[norm ,oo]])
+    assert.is.same('** headline of target custom_id', vim.api.nvim_get_current_line())
+  end)
+
+  it('should follow link to headline of given custom_id in given org file', function()
+    local target_path = helpers.load_file_content({
+      '* Test hyperlink',
+      '  an [[target][internal link]]',
+      '  - some',
+      '  - boiler',
+      '  - plate',
+      '** headline of a deticated anchor',
+      '   - more',
+      '   - boiler',
+      '   - plate <<target>>',
+    })
+    vim.cmd([[norm w]])
+    vim.fn.cursor(2, 30)
+    assert.is.same('  an [[target][internal link]]', vim.api.nvim_get_current_line())
+    vim.cmd([[norm ,oo]])
+    assert.is.same('** headline of a deticated anchor', vim.api.nvim_get_current_line())
+  end)
+
   it('should respect custom  mapping prefix', function()
     config:extend({
       mappings = {
