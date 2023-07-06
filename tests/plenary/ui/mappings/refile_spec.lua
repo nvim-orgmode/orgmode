aeae5c21a7226fa7f7b663b3e710075a500853c5
Sebastian Flügge <seflue@web.de>
2023-07-06 21:53:16 +0200

    chore(tests): break refile tests out of mappings_spec

---
+++ tests/plenary/ui/mappings/refile_spec.lua
@@ -0,0 +1,85 @@
+local helpers = require('tests.plenary.ui.helpers')
+local org = require('orgmode')
+local config = require('orgmode.config')
+local Files = require('orgmode.parser.files')
+
+describe('Refile mappings', function()
+  after_each(function()
+    vim.cmd([[silent! %bw!]])
+  end)
+
+  it('should refile to headline that matches name exactly', function()
+    local destination_file = helpers.load_file_content({
+      '* foobar',
+      '* baz',
+      '** foo',
+    })
+
+    local source_file = helpers.load_file_content({
+      '* to be refiled',
+      '* not to be refiled',
+    })
+
+    source_file = Files.get_current_file()
+    local item = source_file:get_closest_headline()
+    org.instance().capture:refile_to_headline(destination_file, source_file:get_headline_lines(item), item, 'foo')
+    assert.are.same('* not to be refiled', vim.fn.getline(1))
+    vim.cmd('edit' .. vim.fn.fnameescape(destination_file))
+    assert.are.same({
+      '* foobar',
+      '* baz',
+      '** foo',
+      '*** to be refiled',
+    }, vim.api.nvim_buf_get_lines(0, 0, 5, false))
+  end)
+
+  it('should refile to headline and properly demote', function()
+    local destination_file = helpers.load_file_content({
+      '* foobar',
+      '* baz',
+      '** foo',
+    })
+
+    local source_file = helpers.load_file_content({
+      '* to be refiled',
+      '* not to be refiled',
+    })
+
+    source_file = Files.get_current_file()
+    local item = source_file:get_closest_headline()
+    org.instance().capture:refile_to_headline(destination_file, source_file:get_headline_lines(item), item, 'foobar')
+    assert.are.same('* not to be refiled', vim.fn.getline(1))
+    vim.cmd('edit' .. vim.fn.fnameescape(destination_file))
+    assert.are.same({
+      '* foobar',
+      '** to be refiled',
+      '* baz',
+      '** foo',
+    }, vim.api.nvim_buf_get_lines(0, 0, 5, false))
+  end)
+
+  it('should refile to headline and properly promote', function()
+    local destination_file = helpers.load_file_content({
+      '* foobar',
+      '* baz',
+      '** foo',
+    })
+
+    local source_file = helpers.load_file_content({
+      '**** to be refiled',
+      '* not to be refiled',
+    })
+
+    source_file = Files.get_current_file()
+    local item = source_file:get_closest_headline()
+    org.instance().capture:refile_to_headline(destination_file, source_file:get_headline_lines(item), item, 'foobar')
+    assert.are.same('* not to be refiled', vim.fn.getline(1))
+    vim.cmd('edit' .. vim.fn.fnameescape(destination_file))
+    assert.are.same({
+      '* foobar',
+      '** to be refiled',
+      '* baz',
+      '** foo',
+    }, vim.api.nvim_buf_get_lines(0, 0, 5, false))
+  end)
+end)
