31dcefe8a9d62cf8871ebb61e590607af0bae7d1
Sebastian Flügge <seflue@web.de>
2023-07-06 21:06:18 +0200

    chore(tests): break checkbox tests out of mappings_spec

--- tests/plenary/ui/mappings_spec.lua
+++ tests/plenary/ui/mappings_spec.lua
@@ -10,24 +10,6 @@ describe('Mappings', function()
     vim.cmd([[silent! %bw!]])
   end)
 
-  it('should toggle the checkbox state (org_toggle_checkbox)', function()
-    helpers.load_file_content({
-      '* TODO top level todo with multiple tags :OFFICE:PROJECT:',
-      '  - [ ] The checkbox',
-      '  - [X] The checkbox 2',
-      '    - [ ] Nested checkbox',
-    })
-
-    assert.are.same('  - [ ] The checkbox', vim.fn.getline(2))
-    assert.are.same('  - [X] The checkbox 2', vim.fn.getline(3))
-    vim.fn.cursor(2, 1)
-    vim.cmd([[exe "norm \<C-space>"]])
-    assert.are.same('  - [X] The checkbox', vim.fn.getline(2))
-    vim.fn.cursor(3, 1)
-    vim.cmd([[exe "norm \<C-space>"]])
-    assert.are.same('  - [ ] The checkbox 2', vim.fn.getline(3))
-  end)
-
   it('should toggle the current line into a headline and vice versa', function()
     helpers.load_file_content({
       'top level line',
