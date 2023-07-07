921ba501fb125f3bbaf483fb033c6d10d59d856c
Jonas Gollenz <jgollenz@outlook.com>
2023-07-07 22:12:49 +0200

    Insert link after cursor

--- lua/orgmode/objects/help.lua
+++ lua/orgmode/objects/help.lua
@@ -44,6 +44,7 @@ local helps = {
     { key = 'org_forward_heading_same_level', description = 'Go to next heading on same level' },
     { key = 'org_backward_heading_same_level', description = 'Go to previous heading on same level' },
     { key = 'outline_up_heading', description = 'Go to parent heading' },
+    { key = 'org_insert_link', description = 'Insert a hyperlink' },
     { key = 'org_deadline', description = 'Insert/Update deadline date' },
     { key = 'org_schedule', description = 'Insert/Update scheduled date' },
     { key = 'org_time_stamp', description = 'Insert date under cursor' },
