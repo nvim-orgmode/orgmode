36f72b90895b03b57185e0e9bd279a1cda297041
Price Hiller <price@orion-technologies.io>
2023-11-12 16:35:45 -0600

    refactor: extract logic section of indentexpr to top level function
    This allows the block indentation match to call into that function to
    get the future indent for a parent item. This is done so when an indent
    on a visual range is done via `=` it correctly aligns the block with the
    new indentation level of the parent.

--- lua/orgmode/org/indent.lua
+++ lua/orgmode/org/indent.lua
@@ -2,6 +2,92 @@ local config = require('orgmode.config')
 local ts_utils = require('nvim-treesitter.ts_utils')
 local query = nil
 
+local function get_indent_for_linenr(matches, linenr, mode)
+  linenr = linenr or vim.v.lnum
+  mode = mode or vim.fn.mode()
+  local noindent_mode = config.org_indent_mode == 'noindent'
+  local prev_linenr = vim.fn.prevnonblank(linenr - 1)
+  local match = matches[linenr]
+  local prev_line_match = matches[prev_linenr]
+
+  if not match and not prev_line_match then
+    return -1
+  end
+
+  match = match or {}
+  prev_line_match = prev_line_match or {}
+
+  if prev_line_match.type == 'headline' then
+    if noindent_mode or (match.type == 'headline' and match.stars > 0) then
+      return 0
+    end
+    return prev_line_match.indent
+  end
+
+  if match.type == 'headline' then
+    return 0
+  end
+
+  if match.type == 'listitem' then
+    -- We first figure out the indent of the first line of a listitem. Then we
+    -- check if we're on the first line or a "hanging" line. In the latter
+    -- case, we add the overhang.
+    local first_line_indent
+    local parent_linenr = match.nesting_parent_linenr
+    if parent_linenr then
+      local parent_match = matches[parent_linenr]
+      if parent_match.type == 'listitem' then
+        -- Nested listitem. Because two listitems cannot start on the same line,
+        -- we simply fetch the parent's indentation and add its overhang.
+        -- Don't use parent_match.indent, it might be stale if the parent
+        -- already got reindented.
+        first_line_indent = vim.fn.indent(parent_linenr) + parent_match.overhang
+      elseif parent_match.type == 'headline' and not noindent_mode then
+        -- Un-nested list inside a section, indent according to section.
+        first_line_indent = parent_match.indent
+      else
+        -- Noindent mode.
+        first_line_indent = 0
+      end
+    else
+      -- Top-level list before the first headline.
+      first_line_indent = 0
+    end
+    -- Add overhang if this is a hanging line.
+    if linenr ~= match.line_nr then
+      return first_line_indent + match.overhang
+    end
+    return first_line_indent
+  end
+
+  -- In insert mode, we also count the non-listitem line *after* a listitem as
+  -- part of the listitem. Keep in mind that double empty lines end a list as
+  -- per Orgmode syntax.
+  if mode:match('^[iR]') and prev_line_match.type == 'listitem' and linenr - prev_linenr < 3 then
+    -- After the first line of a listitem, we have to add the overhang to the
+    -- listitem's own base indent. After all further lines, we can simply copy
+    -- the indentation.
+    if prev_linenr == prev_line_match.line_nr then
+      return vim.fn.indent(prev_linenr) + prev_line_match.overhang
+    end
+    return vim.fn.indent(prev_linenr)
+  end
+
+  if match.indent_type == 'block' then
+    return match.indent
+  end
+
+  if noindent_mode then
+    return 0
+  end
+
+  if match.indent_type == 'other' then
+    return match.indent
+  end
+
+  return vim.fn.indent(prev_linenr)
+end
+
 local get_matches = ts_utils.memoize_by_buf_tick(function(bufnr)
   local tree = vim.treesitter.get_parser(bufnr, 'org', {}):parse()
   if not tree or not #tree then
@@ -64,7 +150,7 @@ local get_matches = ts_utils.memoize_by_buf_tick(function(bufnr)
         -- If the item is in the block, we shouldn't change the indentation beyond how much we modify the indent of the
         -- block header and footer. This keeps code correctly indented in `BEGIN_SRC` blocks as well as ensuring
         -- `BEGIN_EXAMPLE` blocks don't have their indentation changed inside of them.
-        local parent_indent = vim.fn.indent(parent:start() + 1)
+        local parent_indent = get_indent_for_linenr(matches, parent:start() + 1)
 
         if config.org_indent_mode == 'indent' and parent:type() == 'section' then
           local headline = parent:named_child('headline')
@@ -188,91 +274,10 @@ end
 local function indentexpr(linenr, mode)
   linenr = linenr or vim.v.lnum
   mode = mode or vim.fn.mode()
-  local noindent_mode = config.org_indent_mode == 'noindent'
   query = query or vim.treesitter.query.get('org', 'org_indent')
-
-  local prev_linenr = vim.fn.prevnonblank(linenr - 1)
-
   local matches = get_matches(0)
-  local match = matches[linenr]
-  local prev_line_match = matches[prev_linenr]
 
-  if not match and not prev_line_match then
-    return -1
-  end
-
-  match = match or {}
-  prev_line_match = prev_line_match or {}
-
-  if prev_line_match.type == 'headline' then
-    if noindent_mode or (match.type == 'headline' and match.stars > 0) then
-      return 0
-    end
-    return prev_line_match.indent
-  end
-
-  if match.type == 'headline' then
-    return 0
-  end
-
-  if match.type == 'listitem' then
-    -- We first figure out the indent of the first line of a listitem. Then we
-    -- check if we're on the first line or a "hanging" line. In the latter
-    -- case, we add the overhang.
-    local first_line_indent
-    local parent_linenr = match.nesting_parent_linenr
-    if parent_linenr then
-      local parent_match = matches[parent_linenr]
-      if parent_match.type == 'listitem' then
-        -- Nested listitem. Because two listitems cannot start on the same line,
-        -- we simply fetch the parent's indentation and add its overhang.
-        -- Don't use parent_match.indent, it might be stale if the parent
-        -- already got reindented.
-        first_line_indent = vim.fn.indent(parent_linenr) + parent_match.overhang
-      elseif parent_match.type == 'headline' and not noindent_mode then
-        -- Un-nested list inside a section, indent according to section.
-        first_line_indent = parent_match.indent
-      else
-        -- Noindent mode.
-        first_line_indent = 0
-      end
-    else
-      -- Top-level list before the first headline.
-      first_line_indent = 0
-    end
-    -- Add overhang if this is a hanging line.
-    if linenr ~= match.line_nr then
-      return first_line_indent + match.overhang
-    end
-    return first_line_indent
-  end
-
-  -- In insert mode, we also count the non-listitem line *after* a listitem as
-  -- part of the listitem. Keep in mind that double empty lines end a list as
-  -- per Orgmode syntax.
-  if mode:match('^[iR]') and prev_line_match.type == 'listitem' and linenr - prev_linenr < 3 then
-    -- After the first line of a listitem, we have to add the overhang to the
-    -- listitem's own base indent. After all further lines, we can simply copy
-    -- the indentation.
-    if prev_linenr == prev_line_match.line_nr then
-      return vim.fn.indent(prev_linenr) + prev_line_match.overhang
-    end
-    return vim.fn.indent(prev_linenr)
-  end
-
-  if match.indent_type == 'block' then
-    return match.indent
-  end
-
-  if noindent_mode then
-    return 0
-  end
-
-  if match.indent_type == 'other' then
-    return match.indent
-  end
-
-  return vim.fn.indent(prev_linenr)
+  return get_indent_for_linenr(matches, linenr, mode)
 end
 
 local function foldtext()
