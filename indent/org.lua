be396b94d18e516c8c0fe509e61cfb8983fecf19
Kristijan Husak <husakkristijan@gmail.com>
2024-03-04 21:07:43 +0100

    feat(indent): Add undo_indent variable

--- indent/org.lua
+++ indent/org.lua
@@ -8,3 +8,9 @@ vim.bo.indentexpr = 'v:lua.require("orgmode.org.indent").indentexpr()'
 vim.bo.lisp = false
 vim.bo.smartindent = false
 vim.bo.autoindent = true
+vim.b.undo_indent = table.concat({
+  'setlocal indentexpr<',
+  'lisp<',
+  'smartindent<',
+  'autoindent<',
+}, ' ')
