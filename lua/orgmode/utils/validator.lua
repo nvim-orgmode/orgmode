2e45a6a406bf35820cfc56b5381f615e726a6150
Kristijan Husak <husakkristijan@gmail.com>
2025-01-06 15:38:37 +0100

    fix(tests): Move validator version check outside
    This fixes tests on versions <0.10

--- lua/orgmode/utils/validator.lua
+++ lua/orgmode/utils/validator.lua
@@ -1,10 +1,12 @@
 local M = {}
 
+local has_v_0_11 = vim.fn.has('nvim-0.11') > 0
+
 --- Use the faster validate version if available
 -- Taken from: https://github.com/lukas-reineke/indent-blankline.nvim/pull/934/files#diff-09ebcaa8c75cd1e92d25640e377ab261cfecaf8351c9689173fd36c2d0c23d94R16
 --- @param spec table<string,[any, vim.validate.Validator, boolean|string]>
 function M.validate(spec)
-  if vim.fn.has('nvim-0.11') == 0 then
+  if not has_v_0_11 then
     return vim.validate(spec)
   end
   for key, key_spec in pairs(spec) do
