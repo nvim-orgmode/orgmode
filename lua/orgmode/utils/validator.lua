c6d3dc8c37a641b64b74b8941263c0293935efe9
Kristijan Husak <husakkristijan@gmail.com>
2025-02-03 23:12:51 +0100

    refactor: temporarily disable 0.11 validation

--- lua/orgmode/utils/validator.lua
+++ lua/orgmode/utils/validator.lua
@@ -1,20 +1,20 @@
 local M = {}
 
-local has_v_0_11 = vim.fn.has('nvim-0.11') > 0
+-- local has_v_0_11 = vim.fn.has('nvim-0.11') > 0
 
 --- Use the faster validate version if available
 -- Taken from: https://github.com/lukas-reineke/indent-blankline.nvim/pull/934/files#diff-09ebcaa8c75cd1e92d25640e377ab261cfecaf8351c9689173fd36c2d0c23d94R16
 --- @param spec table<string,[any, vim.validate.Validator, boolean|string]>
 function M.validate(spec)
-  if not has_v_0_11 then
-    return vim.validate(spec)
-  end
-  for key, key_spec in pairs(spec) do
-    local message = type(key_spec[3]) == 'string' and key_spec[3] or nil --[[@as string?]]
-    local optional = type(key_spec[3]) == 'boolean' and key_spec[3] or nil --[[@as boolean?]]
-    ---@diagnostic disable-next-line:param-type-mismatch, redundant-parameter
-    vim.validate(key, key_spec[1], key_spec[2], optional, message)
-  end
+  -- if not has_v_0_11 then
+  return vim.validate(spec)
+  -- end
+  -- for key, key_spec in pairs(spec) do
+  --   local message = type(key_spec[3]) == 'string' and key_spec[3] or nil --[[@as string?]]
+  --   local optional = type(key_spec[3]) == 'boolean' and key_spec[3] or nil --[[@as boolean?]]
+  --   ---@diagnostic disable-next-line:param-type-mismatch, redundant-parameter
+  --   vim.validate(key, key_spec[1], key_spec[2], optional, message)
+  -- end
 end
 
 return M
