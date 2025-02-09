5cd181c19b93f68efc27b1d1dc5a2efae211906c
troiganto <troiganto@proton.me>
2025-02-09 16:16:04 +0100

    feat(attach): add attachment events
    These are implemented in Emacs as the hooks
    `org-attach-after-change-hook` and `org-attach-open-hook`.

--- lua/orgmode/events/types/init.lua
+++ lua/orgmode/events/types/init.lua
@@ -6,4 +6,6 @@ return {
   HeadlinePromoted = require('orgmode.events.types.headline_promoted_event'),
   HeadlineDemoted = require('orgmode.events.types.headline_demoted_event'),
   HeadingToggled = require('orgmode.events.types.heading_toggled'),
+  AttachChanged = require('orgmode.events.types.attach_changed_event'),
+  AttachOpened = require('orgmode.events.types.attach_opened_event'),
 }
