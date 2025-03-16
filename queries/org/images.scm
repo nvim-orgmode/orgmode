11cf2cff3eb9cec96800fc546cf3f022c2bcffe4
Rintaro Okamura <rintaro.okamura@gmail.com>
2025-03-16 23:49:25 +0900

    feat(images): add support for rendering latex math expressions
    adds a treesitter query for representing latex math expressions
    in org code blocks.
    by using snacks.images, they are shown as rendered image.
    
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- queries/org/images.scm
+++ queries/org/images.scm
@@ -2,3 +2,12 @@
   (#gsub! @image.src "^file:" "")
   (#match? @image.src "(png|jpg|jpeg|gif|bmp|webp|tiff|heic|avif|mp4|mov|avi|mkv|webm|pdf)$")
 )
+
+(block
+  name: (expr) @name
+  parameter: (expr) @lang
+  contents: (contents (expr) @image.content)
+  (#match? @name "(src|SRC)")
+  (#eq? @lang "math")
+  (#set! injection.language "latex")
+  (#set! image.ext "math.tex"))
