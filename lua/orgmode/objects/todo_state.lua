2d74d78dfa6c672064ce9570409fa8e2a6fdbd4d
Sebastian Flügge <sebastian.fluegge@dnv.com>
2025-05-03 23:22:56 +0200

    feat: allow to define multiple todo keyword sequences
    They can be defined in the config or within an org file.

--- lua/orgmode/objects/todo_state.lua
+++ lua/orgmode/objects/todo_state.lua
@@ -1,5 +1,4 @@
 local config = require('orgmode.config')
-local utils = require('orgmode.utils')
 local TodoKeyword = require('orgmode.objects.todo_keywords.todo_keyword')
 
 ---@class OrgTodoState
@@ -7,12 +6,29 @@ local TodoKeyword = require('orgmode.objects.todo_keywords.todo_keyword')
 ---@field todos OrgTodoKeywords
 local TodoState = {}
 
----@param data { current_state: string | nil, todos: table | nil }
+---@param data { current_state: string | OrgTodoKeyword | nil, todos: table | nil }
 ---@return OrgTodoState
 function TodoState:new(data)
   local opts = {}
   opts.todos = data.todos or config:get_todo_keywords()
-  opts.current_state = data.current_state and opts.todos:find(data.current_state) or TodoKeyword:empty()
+
+  -- Assign it locally to make the type checker happy.
+  local current_state = data.current_state
+
+  if current_state then
+    -- Find the keyword by string value
+    if type(current_state) == 'string' then
+      opts.current_state = opts.todos:find(current_state) or TodoKeyword:empty()
+    -- Direct assignment of a TodoKeyword
+    elseif type(current_state) == 'table' and current_state.value then
+      opts.current_state = current_state
+    else
+      opts.current_state = TodoKeyword:empty()
+    end
+  else
+    opts.current_state = TodoKeyword:empty()
+  end
+
   setmetatable(opts, self)
   self.__index = self
   return opts
@@ -20,22 +36,56 @@ end
 
 ---@return boolean
 function TodoState:has_fast_access()
-  return self.todos:has_fast_access()
+  -- Enable fast access mode if:
+  -- 1. There are multiple sequences defined, OR
+  -- 2. At least one keyword has an explicit shortcut defined
+  if #self.todos.sequences > 1 or self.todos:has_fast_access() then
+    return true
+  end
+  return false
 end
 
 ---@return OrgTodoKeyword | nil
 function TodoState:open_fast_access()
   local output = {}
-  for _, todo in ipairs(self.todos:all()) do
-    table.insert(output, { '[' })
-    table.insert(output, { todo.shortcut, 'Title' })
-    table.insert(output, { ']' })
-    table.insert(output, { ' ' })
-    table.insert(output, { todo.value, todo.hl })
-    table.insert(output, { '  ' })
+
+  -- Group keywords by sequence
+  local sequences = {}
+  for seq_idx = 1, #self.todos.sequences do
+    sequences[seq_idx] = {}
   end
 
-  table.insert(output, { '\n' })
+  -- Add each keyword to its respective sequence group
+  for _, todo in ipairs(self.todos:all()) do
+    local seq_idx = todo.sequence_index
+    if not sequences[seq_idx] then
+      sequences[seq_idx] = {}
+    end
+
+    local entry = {}
+    table.insert(entry, { '[' })
+    table.insert(entry, { todo.shortcut, 'Title' })
+    table.insert(entry, { ']' })
+    table.insert(entry, { ' ' })
+    table.insert(entry, { todo.value, todo.hl })
+    table.insert(entry, { '  ' })
+
+    table.insert(sequences[seq_idx], entry)
+  end
+
+  -- Display each sequence on a separate line
+  for seq_idx, seq_entries in ipairs(sequences) do
+    -- Flatten the sequence entries
+    for _, entry in ipairs(seq_entries) do
+      for _, part in ipairs(entry) do
+        table.insert(output, part)
+      end
+    end
+
+    -- Add a newline after each sequence (except the last one)
+    table.insert(output, { '\n' })
+  end
+
   vim.api.nvim_echo(output, true, {})
 
   local raw = vim.fn.nr2char(vim.fn.getchar())
@@ -64,22 +114,75 @@ function TodoState:get_prev()
 end
 
 ---@private
----@param direction 1 | -1
+---@param direction number 1 for next, -1 for previous
 ---@return OrgTodoKeyword | nil
 function TodoState:_get_direction(direction)
+  -- When starting from an empty state, get the first/last keyword
   if self.current_state:is_empty() then
-    local keyword = direction == 1 and self.todos:first() or self.todos:last()
+    return self:_handle_empty_state_navigation(direction)
+  end
+
+  -- Get the keyword sequence this state belongs to
+  return self:_navigate_within_sequence(direction)
+end
+
+---@private
+---@param direction number 1 for next, -1 for previous
+---@return OrgTodoKeyword
+function TodoState:_handle_empty_state_navigation(direction)
+  -- When we're starting from an empty state and moving forward,
+  -- go to the first todo keyword of the first sequence
+  if direction == 1 then
+    local keyword = self.todos:first()
+    self.current_state = keyword
+    return keyword
+  -- When we're starting from an empty state and moving backward,
+  -- go to the last todo keyword of the last sequence
+  else
+    local keyword = self.todos:last()
     self.current_state = keyword
     return keyword
   end
+end
 
-  local next_state = self.todos:all()[self.current_state.index + direction]
-  if not next_state then
+---@private
+---@param direction number 1 for next, -1 for previous
+---@return OrgTodoKeyword
+function TodoState:_navigate_within_sequence(direction)
+  -- Get the sequence this keyword belongs to
+  local sequence_idx = self.current_state.sequence_index
+  local seq_keywords = self.todos:sequence(sequence_idx)
+
+  -- Find the position of the current keyword in its sequence
+  local current_idx = nil
+  for idx, keyword in ipairs(seq_keywords) do
+    if keyword.value == self.current_state.value then
+      current_idx = idx
+      break
+    end
+  end
+
+  if not current_idx then
+    -- Fallback to the default behavior if we can't find the keyword in its sequence
+    local next_state = self.todos:all()[self.current_state.index + direction]
+    if not next_state then
+      self.current_state = TodoKeyword:empty()
+      return self.current_state
+    end
+    self.current_state = next_state
+    return next_state
+  end
+
+  -- Get the next keyword in the sequence or cycle to empty
+  local next_idx = current_idx + direction
+  if next_idx < 1 or next_idx > #seq_keywords then
+    -- If we go beyond sequence boundaries, cycle to empty state
     self.current_state = TodoKeyword:empty()
     return self.current_state
   end
-  self.current_state = next_state
-  return next_state
+
+  self.current_state = seq_keywords[next_idx]
+  return self.current_state
 end
 
 ---@param headline OrgHeadline|nil
@@ -93,6 +196,18 @@ function TodoState:get_reset_todo(headline)
     return todo_keyword
   end
 
+  -- If the headline has a current state, use first todo keyword from the same sequence
+  if headline and self.current_state and not self.current_state:is_empty() then
+    local seq_idx = self.current_state.sequence_index
+    local seq = self.todos:sequence(seq_idx)
+    for _, keyword in ipairs(seq) do
+      if keyword.type == 'TODO' then
+        return keyword
+      end
+    end
+  end
+
+  -- Default fallback to the first todo keyword
   return self.todos:first()
 end
 
