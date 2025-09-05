0ab72ada9848455e30da574e4e166b557ffa7509
Kristijan Husak <husakkristijan@gmail.com>
2025-09-05 14:42:49 +0200

    feat(treesitter)!: Use async parsing for custom highlighter (#1029)

--- lua/orgmode/colors/highlighter/init.lua
+++ lua/orgmode/colors/highlighter/init.lua
@@ -5,13 +5,15 @@
 ---@field private todos OrgTodosHighlighter
 ---@field private foldtext OrgFoldtextHighlighter
 ---@field private _ephemeral boolean
----@field private buffers table<number, { language_tree: vim.treesitter.LanguageTree, tree: TSTree }>
+---@field private buffers table<number, { language_tree: vim.treesitter.LanguageTree | nil, tree: TSTree }>
+---@field private parsing table<number, boolean>
 local OrgHighlighter = {}
 
 function OrgHighlighter:new()
   local data = {
     namespace = vim.api.nvim_create_namespace('org_custom_highlighter'),
     buffers = {},
+    parsing = {},
     -- Use ephemeral for highlights. Added to config to allow toggling from tests.
     _ephemeral = true,
   }
@@ -38,29 +40,57 @@ function OrgHighlighter:_setup()
   })
 end
 
-function OrgHighlighter:_on_win(_, _, bufnr, topline, botline)
+---@param bufnr number
+---@param win number
+---@param range { start_line: number, end_line: number } | false
+function OrgHighlighter:_parse_tree(bufnr, win, range)
+  self.parsing[win] = self.parsing[win]
+    or nil
+      == self.buffers[bufnr].language_tree:parse(range, function(_, parsed_trees)
+        self.buffers[bufnr].tree = parsed_trees and parsed_trees[1]
+        if self.parsing[win] then
+          self.parsing[win] = false
+          if vim.api.nvim_win_is_valid(win) then
+            vim.api.nvim__redraw({ win = win, valid = false, flush = false })
+          end
+        end
+      end)
+end
+
+function OrgHighlighter:_on_win(_, win, bufnr, topline, botline)
   local is_org_buffer = vim.bo[bufnr].filetype == 'org'
   if not is_org_buffer then
     return false
   end
-  local parsed_trees = {}
   if not self.buffers[bufnr] then
     self.buffers[bufnr] = { language_tree = vim.treesitter.get_parser(bufnr, 'org') }
-    parsed_trees = self.buffers[bufnr].language_tree:parse()
+    self:_parse_tree(bufnr, win, false)
     self.buffers[bufnr].language_tree:register_cbs({
       on_detach = function(buf)
         self:_on_detach(buf)
       end,
     })
   else
-    parsed_trees = self.buffers[bufnr].language_tree:parse({ topline, botline + 1 })
+    self:_parse_tree(bufnr, win, { topline, botline + 1 })
+    if self.parsing[win] then
+      for line = topline, botline do
+        self:_on_line_impl(bufnr, line, true)
+      end
+      return false
+    end
   end
-  self.buffers[bufnr].tree = parsed_trees and parsed_trees[1]
 end
 
 function OrgHighlighter:_on_line(_, _, bufnr, line)
+  self:_on_line_impl(bufnr, line)
+end
+
+---@param bufnr number
+---@param line number
+---@param use_cache? boolean
+function OrgHighlighter:_on_line_impl(bufnr, line, use_cache)
   if self.buffers[bufnr].tree then
-    self.markup:on_line(bufnr, line, self.buffers[bufnr].tree)
+    self.markup:on_line(bufnr, line, self.buffers[bufnr].tree, use_cache)
     self.stars:on_line(bufnr, line)
     self.foldtext:on_line(bufnr, line)
   end
